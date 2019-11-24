cc9378e487b4fbc50f60decf42a7c24211efa341
Philippe <lipeph99@gmail.com>
2019-11-24 16:23:06 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <bits/stdc++.h>
 #include "book.h"
 #include "tree.h"
 using namespace std;
@@ -24,6 +25,27 @@ int main()
     filenum++;
     book.readData(filename, tree, filenum);
     filenum++;
+    cout << "  ";
+
+    for (int i = 0;i<filenum;i++){
+        cout << "v" << i << "    ";
+    }
+    cout << endl;
     tree.indiceInvertido(tree.getRoot(), filenum);
+    cout << endl;
+
+    vector<float> aux;
+    vector<string> query;
+    query.push_back("a");
+    query.push_back("b");
+
+    cout << "Q:";
+    aux = tree.cousineRanking(tree.getRoot(),filenum,query);
+    cout << endl << endl;
+
+    //sort(aux.begin(),aux.end());
+    for (int i = 0;i<(int)aux.size();i++){
+        cout << "Doc" << i+1 <<" Nota: " << aux[i] << endl;
+    }
     return 0;
 }
